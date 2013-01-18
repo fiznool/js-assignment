6053e450bcfda9576a5c6bb0095b9d4bf339eae8
Tom Spencer <fiznool@gmail.com>
2013-01-18 13:10:14 +0000

    Initial structure

---
+++ app/spec.js
@@ -0,0 +1,9 @@
+define([
+  'app'
+], function(answers) {
+  describe('Best Practices', function() {
+    it('should pass', function() {
+      expect(1).toBe(1);
+    });
+  });
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+var express   = require('express'),
+    fs        = require('fs'),
+   
+    site      = express.createServer(),
+    staticDir = express['static'];
+
+module.exports = function(opts) {
+  opts = opts || {};
+  opts.port = opts.port || 4444;
+  opts.baseDir = opts.baseDir || './';
+  
+  site.configure(function() {
+    [ 'app', 'data', 'vendor' ].forEach(function(dir) {
+      site.use('/' + dir, staticDir(opts.baseDir + dir));
+    });
+    site.use(express.bodyParser());
+  });
+
+  site.get("/", function(req, res) {
+    fs.createReadStream(opts.baseDir + 'app/specrunner.html').pipe(res);
+  });
+
+  site.listen(opts.port);
+
+  fs.readFile(opts.baseDir + 'help.txt', 'utf-8', function(err, f) {
+    console.log(f.replace('{{port}}', opts.port));
+    console.log("\n\nServing at http://localhost:" + opts.port);
+  });
+};
